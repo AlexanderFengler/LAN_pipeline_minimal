1c226ab02425db712d23418e910c1f006a5c4b7d
AlexanderFengler <alexanderfengler@gmx.de>
2023-11-03 17:47:20 -0400

    add variable to config_acct.sh

--- user_configs_examples/config_acct.sh
+++ user_configs_examples/config_acct.sh
@@ -2,6 +2,7 @@
 
 # Basic account settings
 oscar_acct="carney-frankmj-condo"
+also_use_base_acct=true # whether to distribute some jobs to personal acct as well (number of respective jobs assigned in the config_data_generation.sh file)
 bashrc_path=/users/afengler/.bashrc
 conda_env_name=lan_pipe
 project_folder='/users/afengler/data/proj_lan_pipeline_minimal/LAN_pipeline_minimal/'
